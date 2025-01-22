eed49b3d7da1c81c894a7260229870a3603ae5f7
Stronger Alaefule <114509657+king-stronger@users.noreply.github.com>
2025-01-22 19:47:24 +0000

    Replace the createServer function content with the imported router function wrapped in a try catch block

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 import { createServer } from 'node:http';
+import { router } from './router.js';
 
 const port = process.env.PORT;
 const hostname = process.env.HOSTNAME;
 
-const server = createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello world");
+const server = createServer(async (req, res) => {
+    try {
+        router(req, res);
+    } catch (error) {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("Internal Server Error");
+    }
 });
 
 server.listen(port, hostname, () => {
